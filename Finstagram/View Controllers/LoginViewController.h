8ba59189eed311488bde11cab20c1436cd085cd1
Mercy Bickell <mercycat@fb.com>
2020-07-06 16:12:49 -0600

    add login screen

---
+++ Finstagram/View Controllers/LoginViewController.h
@@ -0,0 +1,17 @@
+//
+//  LoginViewController.h
+//  Finstagram
+//
+//  Created by Mercy Bickell on 7/6/20.
+//  Copyright © 2020 mercycat. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface LoginViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Finstagram/View Controllers/LoginViewController.m
@@ -0,0 +1,40 @@
+//
+//  LoginViewController.m
+//  Finstagram
+//
+//  Created by Mercy Bickell on 7/6/20.
+//  Copyright © 2020 mercycat. All rights reserved.
+//
+
+#import "LoginViewController.h"
+
+@interface LoginViewController ()
+@property (weak, nonatomic) IBOutlet UITextField *usernameField;
+@property (weak, nonatomic) IBOutlet UITextField *passwordField;
+
+@end
+
+@implementation LoginViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (IBAction)didTapSignUp:(id)sender {
+}
+
+- (IBAction)didTapLogin:(id)sender {
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
