c3d220ae1a5810e42e22fa4cec2216987f95da10
Mercy Bickell <mercycat@fb.com>
2020-07-08 14:38:53 -0600

    add post details view controller

---
+++ Finstagram/View Controllers/PostDetailsViewController.h
@@ -0,0 +1,19 @@
+//
+//  PostDetailsViewController.h
+//  Finstagram
+//
+//  Created by Mercy Bickell on 7/8/20.
+//  Copyright © 2020 mercycat. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "Post.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface PostDetailsViewController : UIViewController
+@property (nonatomic, strong) Post *post;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Finstagram/View Controllers/PostDetailsViewController.m
@@ -0,0 +1,58 @@
+//
+//  PostDetailsViewController.m
+//  Finstagram
+//
+//  Created by Mercy Bickell on 7/8/20.
+//  Copyright © 2020 mercycat. All rights reserved.
+//
+
+#import "PostDetailsViewController.h"
+#import "DateTools.h"
+
+@interface PostDetailsViewController ()
+@property (weak, nonatomic) IBOutlet UIImageView *postImage;
+@property (weak, nonatomic) IBOutlet UILabel *usernameLabel;
+@property (weak, nonatomic) IBOutlet UILabel *captionLabel;
+@property (weak, nonatomic) IBOutlet UILabel *timeAgoLabel;
+
+@end
+
+@implementation PostDetailsViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    [self initPost];
+}
+
+- (void)initPost {
+    // Set image
+    UIImage *placeholderImage = [UIImage imageNamed:@"image_placeholder"];
+    [self.post.image getDataInBackgroundWithBlock:^(NSData * _Nullable data, NSError * _Nullable error) {
+        if (error) {
+            NSLog(@"Error getting image: %@", error.localizedDescription);
+            [self.postImage setImage: placeholderImage];
+        } else {
+            [self.postImage setImage: [UIImage imageWithData:data]];
+        }
+    }];
+    
+    // Set labels
+    self.usernameLabel.text = self.post.author.username;
+    self.captionLabel.text = self.post.caption;
+    
+    // Format date to show time since posting
+    NSDate *timeCreated = self.post.createdAt;
+    self.timeAgoLabel.text = [NSString stringWithFormat:@"Posted %@ ago", timeCreated.shortTimeAgoSinceNow];
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
