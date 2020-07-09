46c75b299d12d8074927c017480ea2b14eacdb75
Mercy Bickell <mercycat@fb.com>
2020-07-09 15:28:00 -0600

    add collection view cell and profile view controller

---
+++ Finstagram/Views/PostCollectionCell.h
@@ -0,0 +1,17 @@
+//
+//  PostCollectionCell.h
+//  Finstagram
+//
+//  Created by Mercy Bickell on 7/9/20.
+//  Copyright © 2020 mercycat. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface PostCollectionCell : UICollectionViewCell
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Finstagram/Views/PostCollectionCell.m
@@ -0,0 +1,13 @@
+//
+//  PostCollectionCell.m
+//  Finstagram
+//
+//  Created by Mercy Bickell on 7/9/20.
+//  Copyright © 2020 mercycat. All rights reserved.
+//
+
+#import "PostCollectionCell.h"
+
+@implementation PostCollectionCell
+
+@end
